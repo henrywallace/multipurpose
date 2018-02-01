7592b82a8c43febcf6282e64e075424eb8c49fbc
Henry Wallace <henry@henrywallace.io>
2018-02-01 00:38:22 -0500

    main: move durFmt to its own function

--- mp/main.go
+++ mp/main.go
@@ -9,26 +9,34 @@ import (
 	"time"
 )
 
-func main() {
-	if os.Args[1] != "durfmt" {
-		log.Fatalf("this tool doesn't support commands other than durfmt, yet")
+func durFmt(raw ...string) {
+	pat := regexp.MustCompile(`\p{N}+\.?\p{N}+`)
+	for _, r := range raw {
+		dur, err := time.ParseDuration(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		precision := 2
+		pretty := pat.ReplaceAllStringFunc(dur.String(), func(s string) string {
+			t := strings.Split(s, ".")
+			if len(t) == 1 {
+				return s
+			}
+			left, right := t[0], t[1]
+			if len(right) > precision {
+				right = right[:precision]
+			}
+			return left + "." + right
+		})
+		fmt.Println(pretty)
 	}
-	dur, err := time.ParseDuration(os.Args[2])
-	if err != nil {
-		log.Fatal(err)
-	}
+}
 
-	precision := 2
-	x := regexp.MustCompile(`\p{N}+\.?\p{N}+`).ReplaceAllStringFunc(dur.String(), func(s string) string {
-		t := strings.Split(s, ".")
-		if len(t) == 1 {
-			return s
-		}
-		left, right := t[0], t[1]
-		if len(right) > precision {
-			right = right[:precision]
-		}
-		return left + "." + right
-	})
-	fmt.Println(x)
+func main() {
+	switch os.Args[1] {
+	case "durfmt":
+		durFmt(os.Args[2])
+	default:
+		log.Fatalf("command not supported: %s", os.Args[1])
+	}
 }
