592d43daa77e39e539d8e129de480b120b7ef93e
Henry Wallace <henry@henrywallace.io>
2018-02-01 21:19:43 -0500

    add counter function for words

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Simple swiss-army knife go tool `mp` for various tasks while working the shell.
 `mp durfmt DURATION` - parse a time duration and output a time.Duration parsed
 version with each part (e.g. seconds) truncated to %.2f precision.
 
+`curl http://theinter.net/book.txt | mp counter | head -10` - count the
+top 10 words via strings.Fields on input.
+
 TODO:
   - switch git branches
   - show git branch status
-  - top words on piped input (optional remove stop words)
+  - add option to remove stop words in counter
+  - add option to do ngrams in counter
--- mp/main.go
+++ mp/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
 )
@@ -32,10 +34,81 @@ func durFmt(raw ...string) {
 	}
 }
 
+func counter(top int, words ...string) {
+	type wordCount struct {
+		word  string
+		count int
+	}
+	counts := make(map[string]int)
+	for _, word := range words {
+		counts[word]++
+	}
+	var wcs []wordCount
+	for word, count := range counts {
+		wcs = append(wcs, wordCount{
+			word:  word,
+			count: count,
+		})
+	}
+
+	sort.Slice(wcs, func(i, j int) bool {
+		return wcs[i].count > wcs[j].count
+	})
+
+	for i, wc := range wcs {
+		if top > -1 && i >= top {
+			break
+		}
+		fmt.Printf("%-16s  %d\n", wc.word, wc.count)
+	}
+}
+
+func tokenize(lines ...string) []string {
+	var tokens []string
+	for _, line := range lines {
+		tokens = append(tokens, strings.Fields(line)...)
+	}
+	return tokens
+}
+
+func isPipe() bool {
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		return true
+	}
+	return false
+}
+
+func scanArgsOrStdin() []string {
+	var out []string
+
+	if isPipe() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			out = append(out, scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if len(os.Args) > 2 {
+			for _, word := range os.Args[2:] {
+				out = append(out, word)
+			}
+		}
+	}
+
+	return out
+}
+
 func main() {
 	switch os.Args[1] {
 	case "durfmt":
 		durFmt(os.Args[2])
+	case "counter":
+		input := scanArgsOrStdin()
+		tokens := tokenize(input...)
+		counter(-1, tokens...)
 	default:
 		log.Fatalf("command not supported: %s", os.Args[1])
 	}
